37936cb1fd2fcace48d9dfef1f80caebe8cdfe0d
Edwin Carrasquillo <ecarrasquillo@Edwins-MacBook-Pro.local>
2016-12-22 20:09:10 -0500

    added the use of regex to compile text data, user lookup and two groups of messages to be picked at random depending on username

--- plugins/motivation/motivation.py
+++ plugins/motivation/motivation.py
@@ -1,13 +1,56 @@
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from rtmbot.core import Plugin
+import random
+import re
+from slackclient import SlackClient
 outputs = []
 crontable = []
 
-from rtmbot.core import Plugin
+trigger_message = re.compile(r'^!m\s?(\<@(?P<user>\w+)\>)?', re.I)
+
+congratulations = [
+    "Great job",
+    "Amazing,",
+    "You rock",
+    "OMG",
+    "You're awesome,"
+]
+
+group_pronouns = [
+    "everyone",
+    "everybody",
+    "y'all",
+    "mortals",
+    "peeps",
+    "folks",
+    "minions"
+]
 
 class MotivationPlugin(Plugin):
+    
+    def _lookup_user(user):
+        return slack_client.api_call(
+            'users.info',
+            user=user
+            )
 
-	def process_message(self, data):
-		if data['text'].lower().startswith("!m"):
-			self.outputs.append([
-				data['channel'],
-				"Great job!"
-				])
+    def process_message(self, data):
+        catch = trigger_message.match(data['text'])
+        if catch:
+            username = catch.groupdict()['user']
+            if username is not None:
+                user = _lookup_user(username)
+                message = "@{}".format(user['user']['name'])
+            else:
+                text = data['text'][3:]
+                message = text or random.choice(group_pronouns)
+            self.outputs.append([
+                data["channel"],
+                ":tada: {} {}! :tada:".format(
+                    random.choice(congratulations),
+                    message
+                    )
+                ])
